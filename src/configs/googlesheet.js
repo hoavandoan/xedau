eab527ea8ef098f3c693badb3a137f9dec8bdab9
doanhv <hoavandoan@gmail.com>
2019-12-25 00:35:56 +0700

    fix data, modal

--- src/configs/googlesheet.js
+++ src/configs/googlesheet.js
@@ -2,14 +2,14 @@ import _ from 'lodash'
 
 export function getData() {
     const spreadsheetId = '1dWcQSzmeFpVPNno01WXovj6072xW0JbRQzg0IVjYFbQ'
-    const range = 'Sheet1!E5:N6'
+    const range = 'Sheet1!E5:R8'
     const url = `https://sheets.googleapis.com/v4/spreadsheets/${spreadsheetId}/values/${range}?key=AIzaSyA-UXoZqJhkIXOK0RaGOQXAubeX1QXAUSM`
     const data = []
     return fetch(url)
         .then(response=> response.json())
         .then(res=>{
             const data = res.values
-            const infoCar = ['carOwner','phoneNumber','address','price','timeStart','timeEnd','image','category','note','favorite']
+            const infoCar = ['carOwner','phoneNumber','address','from','to','price','timeStart','timeEnd','timeGoBackStart','timeGoBackEnd','image','category','note','favorite']
             return data.map(item =>{
                 return _.zipObject(infoCar,item)
             })
